cde0b58b4d01c52e5ec7806008de7827885fe6de
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-07-19 22:29:40 +0300

    Update User model

--- models/User.js
+++ models/User.js
@@ -3,33 +3,15 @@ const mongoose = require('mongoose')
 //User schema
 const userSchema = new mongoose.Schema(
   {
-    firstName: {
-      type: String,
-      required: true
-    },
-    lastName: {
-      type: String,
-      required: true
-    },
     username: {
       type: String,
       required: true,
-      minLength: 4,
-      maxLenght: 20,
-      unique: true
+      unique: true,
+      trim: true
     },
     password: {
       type: String,
-      required: true,
-      minLength: 8,
-      maxLenght: 20
-    },
-    profileImage: {
-      type: String
-    },
-    isAdmin: {
-      type: Boolean
-      // required: true
+      required: true
     }
   },
   { timestamps: true }
