bf834904c6138b61d802c4a7f72bb0d8b57918ae
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-07-20 21:44:22 +0300

    Update create routine

--- controllers/routine.controllers.js
+++ controllers/routine.controllers.js
@@ -1,15 +1,35 @@
 const express = require('express')
+const isSignedIn = require('../middleware/is-signed-in')
 const router = express.Router()
+const Exercise = require('../models/Exercise')
+const Workout = require('../models/Workout')
 
-router.get('/', (req, res) => {
-  res.send('All routines')
+//list the user workout routines
+router.get('/', isSignedIn, async (req, res) => {
+  try {
+    const routines = await Workout.find({ owner: req.session.user._id })
+  } catch (err) {
+    console.log('ERROR in Listing the Workout routines')
+    res.redirect('/')
+  }
 })
 
-router.get('/new', (req, res) => {
-  res.render('../views/createRoutine.ejs')
+//get: form to create routine
+router.get('/new', isSignedIn, (req, res) => {
+  res.render('../views/routine/createRoutine.ejs')
 })
-router.post('/', (req, res) => {
-  res.send('Routine is created')
+
+//post: create routine
+router.post('/', isSignedIn, async (req, res) => {
+  try {
+    req.body.owner = req.session.user._id
+    req.body.isPublic = req.body.isPublic === 'on'
+    await Wourkout.create(req.body)
+    res.redirect('/routines')
+  } catch (err) {
+    console.log('ERROR in creating Workout routines')
+    res.redirect('/routines/new')
+  }
 })
 router.get('/:id', (req, res) => {
   res.send(`Routines ${req.params.id}`)
