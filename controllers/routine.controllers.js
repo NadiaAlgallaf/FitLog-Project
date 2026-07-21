d4d69c3633a2eb2e03fc5c2f5e5152bb61527efa
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-07-21 20:17:55 +0300

    Create routine functionality

--- controllers/routine.controllers.js
+++ controllers/routine.controllers.js
@@ -1,41 +1,54 @@
 const express = require('express')
 const isSignedIn = require('../middleware/is-signed-in')
 const router = express.Router()
-const Exercise = require('../models/Exercise')
+
 const Workout = require('../models/Workout')
+const WorkoutExercise = require('../models/WorkoutExercise')
 
 //list the user workout routines
-router.get('/', isSignedIn, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const routines = await Workout.find({ owner: req.session.user._id })
-    res.render('routine/indexRoutine.ejs')
+    res.render('routine/indexRoutine.ejs', { routines })
   } catch (err) {
-    console.log('ERROR in Listing the Workout routines')
+    console.log('ERROR in Listing the Workout routines', err)
     res.redirect('/')
   }
 })
 
 //get: form to create routine
-router.get('/new', isSignedIn, async (req, res) => {
-  const allExercises = await Exercise.find()
-  res.render('routine/StartRoutine.ejs', { exercises: allExercises })
+router.get('/new', (req, res) => {
+  res.render('routine/StartRoutine.ejs')
 })
 
-router.post('/new', isSignedIn, async (req, res) => {
+//post: create routine
+router.post('/', async (req, res) => {
   try {
     req.body.owner = req.session.user._id
     req.body.isPublic = req.body.isPublic === 'on'
+
     const createdWorkout = await Workout.create(req.body)
     res.redirect('/routines/add-exercises/' + createdWorkout._id)
   } catch (err) {
     console.log('ERROR in creating Workout routines', err)
   }
 })
-router.get('/add-exercises/:id', (req, res) => {
-  // 1. get all exercises from the database
-  // 2. get the workout with the id from req.params
-  // 3. pass both to the ejs page
-  res.render('routine/createRoutine.ejs')
+
+//get: show one routine (to add exercises)
+router.get('/add-exercises/:id', async (req, res) => {
+  try {
+    // 1. get all exercises from the database
+    const exercises = await Exercise.find()
+
+    // 2. get the workout with the id from req.params
+    const workout = await Workout.findById(req.params.id)
+
+    // 3. pass both to the ejs page
+    res.render('routine/createRoutine.ejs', exercises, workout)
+  } catch (err) {
+    console.log(err)
+    res.redirect('/routines')
+  }
 })
 
 router.post('/add-exercises/:id', (req, res) => {
