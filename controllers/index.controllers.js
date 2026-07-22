ed1363942a4f75d9562e9eff36857c5ebadba11d
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-07-22 18:15:18 +0300

    Add public routines route

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -4,4 +4,22 @@ const Workout = require('../models/Workout')
 router.get('/', (req, res) => {
   res.render('homepage.ejs')
 })
+
+router.get('/public-routines', async (req, res) => {
+  try {
+    const routines = await Workout.find({
+      isPublic: true
+    })
+      .populate('owner', 'username')
+      .sort({ createdAt: -1 })
+
+    res.render('public-routines.ejs', {
+      routines
+    })
+  } catch (error) {
+    console.log('Error loading public routines:', error)
+    res.redirect('/')
+  }
+})
+
 module.exports = router
